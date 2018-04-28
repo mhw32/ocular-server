7ec3d671ec70c79913595c9ec78e5b205aa8f989
Mike Wu <me@mikewuis.me>
2018-04-28 13:39:31 -0700

    Eyepiece parts working

--- ocular/glasses.py
+++ ocular/glasses.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import os
+import copy
 import numpy as np
 from PIL import Image
 
@@ -38,28 +39,86 @@ class Glasses(object):
         # NOTE: only call after <load_pieces_from_directory>
         left_eyepiece = self._place_left_eyepiece(face)
         right_eyepiece = self._place_right_eyepiece(face)
-        left_earpiece = self._place_left_earpiece(face, left_eyepiece)
-        right_earpiece = self._place_right_earpiece(face, right_eyepiece)
-        center_piece = self._place_center_piece(face, left_eyepiece, right_earpiece)
+        # left_earpiece = self._place_left_earpiece(face, left_eyepiece)
+        # right_earpiece = self._place_right_earpiece(face, right_eyepiece)
+        # center_piece = self._place_center_piece(face, left_eyepiece, right_earpiece)
         return {
             'left_eyepiece': left_eyepiece,
             'right_eyepiece': right_eyepiece,
-            'left_earpiece': left_earpiece,
-            'right_earpiece': right_earpiece,
-            'center_piece': center_piece,
+            # 'left_earpiece': left_earpiece,
+            # 'right_earpiece': right_earpiece,
+            # 'center_piece': center_piece,
         }
 
-    def _place_left_eyepiece(self, face):
-        assert self.left_earpiece is not None
+    def _place_left_eyepiece(self, face, width_factor=1.75):
+        assert self.left_eyepiece is not None
+        eyepiece = copy.deepcopy(self.left_eyepiece)
+        eye = face['keypoints'][36:42]
+        # compute center of eye
+        eye_center = np.round(np.mean(eye, axis=0)).astype(np.int)
+        eye_width = np.max(eye[:, 0]) - np.min(eye[:, 0])
 
-    def _place_right_eyepiece(self, face):
+        # compute size with assumption of the width of a glasses is 
+        # 2x the width of eye. This should be make tunable
+        old_eyepiece_width, old_eyepiece_height = eyepiece.size
+        new_eyepiece_width = int(np.round(eye_width * width_factor))
+        new_eyepiece_height = int(np.round(new_eyepiece_width / float(old_eyepiece_width) *  old_eyepiece_height))
+
+        # things are easier if sizes are all old
+        if new_eyepiece_width % 2 == 0:
+            new_eyepiece_width += 1
+        if new_eyepiece_height % 2 == 0:
+            new_eyepiece_height += 1
+
+        # reshape eyepiece
+        eyepiece = eyepiece.resize((new_eyepiece_width, new_eyepiece_height))
+        eyepiece = np.asarray(eyepiece)
+
+        return {
+            'data': eyepiece,
+            'center': eye_center,
+            'loc': (int(eye_center[1] - (new_eyepiece_height - 1) / 2), 
+                    int(eye_center[0] - (new_eyepiece_width - 1) / 2),
+                    new_eyepiece_height, new_eyepiece_width)
+        }
+
+    def _place_right_eyepiece(self, face, width_factor=1.75):
         assert self.right_earpiece is not None
+        eyepiece = copy.deepcopy(self.right_eyepiece)
+        eye = face['keypoints'][42:48]
+        # compute center of eye
+        eye_center = np.round(np.mean(eye, axis=0)).astype(np.int)
+        eye_width = np.max(eye[:, 0]) - np.min(eye[:, 0])
 
-    def _place_left_earpiece(self, face, left_eyepiece):
-        assert self.left_earpiece is not None
+        # compute size with assumption of the width of a glasses is 
+        # 2x the width of eye. This should be make tunable
+        old_eyepiece_width, old_eyepiece_height = eyepiece.size
+        new_eyepiece_width = int(np.round(eye_width * width_factor))
+        new_eyepiece_height = int(np.round(new_eyepiece_width / float(old_eyepiece_width) *  old_eyepiece_height))
 
-    def _place_right_earpiece(self, face, right_eyepiece):
-        assert self.right_earpiece is not None
+        # things are easier if sizes are all old
+        if new_eyepiece_width % 2 == 0:
+            new_eyepiece_width += 1
+        if new_eyepiece_height % 2 == 0:
+            new_eyepiece_height += 1
+
+        # reshape eyepiece
+        eyepiece = eyepiece.resize((new_eyepiece_width, new_eyepiece_height))
+        eyepiece = np.asarray(eyepiece)
+
+        return {
+            'data': eyepiece,
+            'center': eye_center[::-1],
+            'loc': (int(eye_center[1] - (new_eyepiece_height - 1) / 2),
+                    int(eye_center[0] - (new_eyepiece_width - 1) / 2), 
+                    new_eyepiece_height, new_eyepiece_width)
+        }
+
+    # def _place_left_earpiece(self, face, left_eyepiece):
+    #     assert self.left_earpiece is not None
+
+    # def _place_right_earpiece(self, face, right_eyepiece):
+    #     assert self.right_earpiece is not None
 
     def _place_center_piece(self, face, left_eyepiece, right_eyepiece):
         assert self.center_piece is not None
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import cv2
+import numpy as np
 from ocular import ocular
 from ocular.glasses import Glasses
 
@@ -21,11 +22,19 @@ if __name__ == "__main__":
 
         faces = ocular.get_facial_keypoints_from_frame(frame)
         for (i, face) in enumerate(faces):
-            (x, y, w, h) = face['bbox']
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            pieces = dummy.place_glasses(face)
+            for piece in pieces.itervalues():
+                x, y, h, w = piece['loc']
+                alpha_s = piece['data'][:, :, 3] / 255.0
+                alpha_l = 1.0 - alpha_s
 
-            for (x, y) in face['keypoints']:
-                cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+                for c in xrange(0, 3):
+                    frame_slice = np.round((alpha_s * piece['data'][:, :, c] + 
+                                            alpha_l * frame[x:x+h, y:y+w, c]))
+                    frame[x:x+h, y:y+w, c] = frame_slice.astype(np.uint8)
+
+                for (x, y) in face['keypoints']:
+                    cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
 
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
